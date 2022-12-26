19c7eb50b45e24f808d37ed027cdddb851098b5e
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2022-12-26 22:14:43 +0530

    ok

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const transactionController = require("../controllers/transactionController");
 
-router.post("/success", transactionController.successfulTransaction);
+router.post("/success/:fName", transactionController.successfulTransaction);
 
 module.exports = router;
