79a8140032f832d8f14dcc890f4475ca45a430e0
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2022-12-26 20:58:57 +0530

    parse

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
