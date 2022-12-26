4998ca807864a15618956cd4811e61540890b152
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2022-12-26 17:52:51 +0530

    decryption

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,7 +1,22 @@
+const merchantkKey = "vT11bhGTmZHslsUNYl1Mh9H/wMuuKww/Mo7gaoe8YBg=";
+
 const transacionController = {
   successfulTransaction: async (req, res) => {
+    function decrypt(text, skey) {
+      var base64Iv = "0123456789abcdef";
+      var key = CryptoJS.enc.Base64.parse(skey);
+      var iv = CryptoJS.enc.Utf8.parse(base64Iv);
+      var decrypted = CryptoJS.AES.decrypt(text, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7,
+      });
+      var decryptedData = decrypted.toString(CryptoJS.enc.Utf8);
+      return decryptedData;
+    }
     const payload = req.body;
-    res.send(payload);
+    response = decrypt(payload, merchantkKey);
+    res.send(response);
   },
 };
 
