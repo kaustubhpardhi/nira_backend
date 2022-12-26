9c24c44418f0a42e56edf2132fd786075c3ce6ff
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2022-12-26 22:40:23 +0530

    ok

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -28,7 +28,7 @@ const transactionController = {
     const data = req.body;
     // const response = decrypt(data.txn_response, merchantKey);
     // console.log(response);
-    res.send("heylo");
+    res.sendFile(__dirname + "/index.html");
     console.log(data.txn_response);
   },
 };
