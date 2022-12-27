ece32ea837a96fd91d20bf44c5dbbf59a42b7240
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2022-12-27 12:28:29 +0530

    ok

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -29,90 +29,90 @@ const transactionController = {
     // const response = decrypt(data.txn_response, merchantKey);
     // console.log(response);
     //res.send("hey");
-    res.sendFile(__dirname + "/index.html");
-    //   res.send(`<div
-    //   class="pdfBody"
-    //   style="
-    //     padding-top: 4px;
-    //     padding-left: 6px;
-    //     background: rgb(255, 255, 153);
-    //     border: 1px solid black;
-    //     width: 50%;
-    //   "
-    // >
-    //   <h5
-    //     style="
-    //       color: #8c2d29;
-    //       font-weight: 700;
-    //       text-align: center;
-    //       margin-top: 3px;
-    //     "
-    //   >
-    //     Shri Laxmi Narasinha Deosthan Trust Nira Narasingpur
-    //   </h5>
-    //   <div
-    //     class="printFlex"
-    //     style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 3px"
-    //   >
-    //     <div>
-    //       <img src="images/nira2.jpeg" alt="GodImage" />
-    //       <p class="pdfAmount" style="font-size: 24px; text-align: center">
-    //         <b>Amount : </b> &#x20B9;
-    //       </p>
-    //     </div>
-    //     <div>
-    //       <table style="text-align: left" class="pdfTable">
-    //         <tbody>
-    //           <tr>
-    //             <th>Receipt No</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Receipt Date</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Name</th>
-    //             <td>${req.params.fName}</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Gotra</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Date</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>State</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Mobile</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Email</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>PAN</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Aadhar</th>
-    //             <td>:</td>
-    //           </tr>
-    //           <tr>
-    //             <th>Purpose</th>
-    //             <td>:</td>
-    //           </tr>
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //   </div>
-    // </div>
-    // `);
+    // res.sendFile(__dirname + "/index.html");
+    res.send(`<div
+    class="pdfBody"
+    style="
+      padding-top: 4px;
+      padding-left: 6px;
+      background: rgb(255, 255, 153);
+      border: 1px solid black;
+      width: 50%;
+    "
+  >
+    <h5
+      style="
+        color: #8c2d29;
+        font-weight: 700;
+        text-align: center;
+        margin-top: 3px;
+      "
+    >
+      Shri Laxmi Narasinha Deosthan Trust Nira Narasingpur
+    </h5>
+    <div
+      class="printFlex"
+      style="display: grid; grid-template-columns: repeat(2, 1fr); gap: 3px"
+    >
+      <div>
+        <img src="https://drive.google.com/file/d/1x1gyir0VHW018wd8Mfwa6zsHsm6EmqxW/view?usp=share_link" alt="GodImage" />
+        <p class="pdfAmount" style="font-size: 24px; text-align: center">
+          <b>Amount : </b> &#x20B9;
+        </p>
+      </div>
+      <div>
+        <table style="text-align: left" class="pdfTable">
+          <tbody>
+            <tr>
+              <th>Receipt No</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Receipt Date</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Name</th>
+              <td>${req.params.fName}</td>
+            </tr>
+            <tr>
+              <th>Gotra</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Date</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>State</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Mobile</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>PAN</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Aadhar</th>
+              <td>:</td>
+            </tr>
+            <tr>
+              <th>Purpose</th>
+              <td>:</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  `);
     console.log(data.txn_response);
   },
 };
