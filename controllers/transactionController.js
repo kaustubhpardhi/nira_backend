7a5c4f22e72737764119b5ad4147cbab8c3a416d
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-01-23 16:14:58 +0530

    TransactionController - updated index.html

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -147,7 +147,7 @@ const transactionController = {
 </div
           </div>
           <div style="text-align:center;padding-bottom:10px; margin-top:15px;">
-          <button onclick="window.print()" style="background: rgb(255, 255, 153); border: 1px solid black; color: #8c2d29; font-weight: 700;height: 40px; width: 120px;">Print Receipt</button>
+          <button onclick="window.print()" style="background: rgb(255, 255, 153); border: 1px solid black; color: #8c2d29; font-weight: 700;height: 40px; width: 120px;">Download Receipt</button>
           </div>
         `);
       }
